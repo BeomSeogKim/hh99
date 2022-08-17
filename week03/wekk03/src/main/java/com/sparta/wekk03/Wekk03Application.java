840ad6cb6e61da3abfc31aadb1945769578a5793
BeomSeogKim <kbs4520@daum.net>
2022-08-17 14:20:31 +0900

    intializing

---
+++ week03/wekk03/src/main/java/com/sparta/wekk03/Wekk03Application.java
@@ -0,0 +1,13 @@
+package com.sparta.wekk03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wekk03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wekk03Application.class, args);
+    }
+
+}
